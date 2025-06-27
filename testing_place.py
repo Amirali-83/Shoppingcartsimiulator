4a90b19bd965a967b43cad5238267f1ec92fa666
Amirali-83 <amirali.sotoudeh@yahoo.com>
2025-06-27 14:02:02 +0200

    Update testing_place.py

--- testing_place.py
+++ testing_place.py
@@ -1,11 +1,1 @@
-import csv
 
-
-product_mobile = []
-with open('products.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if row['Category'] == 'Mobile':
-            product_mobile.extend([row['Category'], row['ID']])
-
-print(product_mobile)
